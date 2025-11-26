8a41b0c8964b97806b5072403fc4162daf284022
Christina42-e <kristinakattel45@gmail.com>
2025-11-26 07:35:14 +0545

    returning 5

--- arrfunc.c
+++ arrfunc.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-void myfunction(int arrfunc[5])
-{
-    for(int i = 0; i<5 ; i++)
-    printf("%d\n", arrfunc[i]);
+#include <stdio.h>
 
+void myFunction(int myNumbers[5]) {
+  for (int i = 0; i < 5; i++) {
+    printf("%d\n", myNumbers[i]);
+  }
 }
-int main()
-{
-    int arrfunc[5] = {10, 20, 30, 40, 50};
-    myfunction(arrfunc);
-    return 0;
+
+int main() {
+  int myNumbers[5] = {10, 20, 30, 40, 50};  
+  myFunction(myNumbers);
+  return 0;
 }
---
+++ funct.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int myfunction(int x)
+{ 
+ return 5 + x;
+}
+int main()
+{
+    printf("%d\n", myfunction(3));
+    return 0;
+}
